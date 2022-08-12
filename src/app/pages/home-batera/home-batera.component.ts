b8851b7930b5c49aefa2205abe768c69dd7597bb
Dimasariza <roganda.sihombing11@gmail.com>
2022-08-12 21:49:35 +0700

    Menambahkan logo header dan memperbarui menu home

--- src/app/pages/home-batera/home-batera.component.ts
+++ src/app/pages/home-batera/home-batera.component.ts
@@ -11,35 +11,36 @@ export class HomeBateraComponent implements OnInit {
 
   data = [
     {
-      imageSrc : 'https://images.unsplash.com/photo-1606185540834-d6e7483ee1a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      imageAlt : '1',
-      shipName : 'Batera Ship 1'
+      imageSrc : '../../../assets/images/Ship/KM SALMON MUSTAFA.png',
+      imageAlt : 'MT SALMON MUSTAFA SHIP',
+      shipName : 'MT SALMON MUSTAFA'
     },
     {
-      imageSrc : 'https://images.unsplash.com/photo-1585713181935-d5f622cc2415?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
-      imageAlt : '2',
-      shipName : 'Batera Ship 2'
+      imageSrc : 'https://www.pupuk-indonesia.com/public/uploads/2020/11/IMG_20181117_202559_3241605578072_berita.jpg',
+      imageAlt : 'MT SULTAN MAHMUD BADARUDDIN II SHIP',
+      shipName : 'MT SULTAN MAHMUD BADARUDDIN II'
     },
     {
-      imageSrc : 'https://images.unsplash.com/photo-1524522173746-f628baad3644?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1231&q=80',
-      imageAlt : '3',
-      shipName : 'Batera Ship 3'
+      imageSrc : '../../../assets/images/Ship/KM PUSRI INDONESIA I.png',
+      imageAlt : 'KM PUSRI INDONESIA I SHIP',
+      shipName : 'KM PUSRI INDONESIA I'
 
     },
     {
-      imageSrc : 'https://images.unsplash.com/photo-1530890448995-4d82724f702c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
-      imageAlt : '4',
-      shipName : 'Batera Ship 4'
+      imageSrc : '../../../assets/images/Ship/KM ABUSAMAH.png',
+      imageAlt : 'KM ABUSAMAH SHIP',
+      shipName : 'KM ABUSAMAH'
     },
     {
-      imageSrc : 'https://images.unsplash.com/photo-1542986386-660ccbbedaf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      imageAlt : '5',
-      shipName : 'Batera Ship 5'
+      
+      imageSrc : '../../../assets/images/Ship/KM IBRAHIM ZAHIER.png',
+      imageAlt : 'KM IBRAHIM ZAHIER SHIP',
+      shipName : 'KM IBRAHIM ZAHIER'
     },
     {
-      imageSrc : 'https://images.unsplash.com/photo-1606185540834-d6e7483ee1a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      imageAlt : '6',
-      shipName : 'Batera Ship 6'
+      imageSrc : '../../../assets/images/Ship/KM JULIANTO.png',
+      imageAlt : 'KM JULIANTO MOELIODIHARDJO SHIP',
+      shipName : 'KM JULIANTO MOELIODIHARDJO'
     },
   ]
 
