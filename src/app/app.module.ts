62725c941aa5abf5d85ba84dd84dab902047988b
wida praponco <widapraponco02@gmail.com>
2022-09-14 21:14:25 +0700

    add http interceptor to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import {
   NbToastrModule,
   NbWindowModule,
 } from '@nebular/theme';
+import { AuthInterceptor } from './http-interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -40,6 +41,9 @@ import {
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
     NbDatepickerModule.forRoot()  
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
 })
