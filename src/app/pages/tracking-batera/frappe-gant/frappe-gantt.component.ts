b366272c2751566c688ca277c8024ad991b8eb9b
wida praponco <widapraponco02@gmail.com>
2022-10-23 02:20:53 +0700

    add enable scurve in frappe gantt

--- src/app/pages/tracking-batera/frappe-gant/frappe-gantt.component.ts
+++ src/app/pages/tracking-batera/frappe-gant/frappe-gantt.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
 import Gantt from './lib'
 
 @Component({
@@ -7,8 +7,11 @@ import Gantt from './lib'
     styleUrls: ['./frappe-gantt.component.scss'],
 })
 
-export class FrappeGanttComponent implements OnInit {
+export class FrappeGanttComponent implements OnInit, OnChanges {
     @ViewChild('gantt') ganttEl: ElementRef;
+
+    @Input() showSCurve: boolean = false
+    @Input() viewMode: string = 'Day'
 
     gantt;
     tasks = [
@@ -17,8 +20,11 @@ export class FrappeGanttComponent implements OnInit {
             name: 'Redesign website',
             start: '2016-1-11',
             end: '2016-1-21',
+            price: 500,
             progress_log: [
-                {progress: 100, date: '2016-1-15'},
+                {progress: 10, date: '2016-1-12'},
+                {progress: 50, date: '2016-1-15'},
+                {progress: 100, date: '2016-1-30'},
             ],
             // dependencies: 'Task 2, Task 3',
         },
@@ -27,6 +33,7 @@ export class FrappeGanttComponent implements OnInit {
             name: 'Redesign website 2',
             start: '2016-1-21',
             end: '2016-1-25',
+            price: 300,
             progress_log: [
                 {progress: 100, date: '2016-1-24'}
             ],
@@ -37,16 +44,55 @@ export class FrappeGanttComponent implements OnInit {
             name: 'Redesign website 3',
             start: '2016-1-25',
             end: '2016-1-31',
+            price: 200,
             progress_log: [
-                {progress: 100, date: '2016-1-30'},
+                {progress: 100, date: '2016-1-31'},
             ],
             dependencies: 'Task 2',
         },
+        {
+            id: 'Task 4',
+            name: 'Redesign website',
+            start: '2016-1-11',
+            end: '2016-1-21',
+            price: 100,
+            progress_log: [
+                {progress: 50, date: '2016-1-15'},
+                {progress: 100, date: '2016-1-21'},
+            ],
+            // dependencies: 'Task 2, Task 3',
+        },
     ].map(t => {
-        return {...t, progress: t.progress_log.map(p => p.progress).reduce((p,c) => p+c, 0)/(t.progress_log?.length ?? 1)}
+        return {...t, progress: t.progress_log?.map(p => p.progress).reduce((p,c) => p+c, 0)/(t.progress_log?.length ?? 1) ?? 0}
     });
 
     ngOnInit() {
-        this.gantt = new Gantt('#gantt', this.tasks, {});
+        this.gantt = new Gantt('#gantt', this.tasks, {});        
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        // console.log(changes);
+        
+        const {showSCurve, viewMode} = changes
+
+        if (showSCurve && showSCurve.previousValue != showSCurve.currentValue) {
+            this.toggleSCurve()
+        }
+
+        if (viewMode && viewMode.previousValue != viewMode.currentValue) {
+            this.changeViewMode()
+        }
+    }
+
+    changeViewMode() {
+        if (this.gantt) {
+            this.gantt.change_view_mode(this.viewMode)
+        }
+    }
+
+    toggleSCurve() {        
+        if (this.gantt) {
+            this.gantt.toggle_scurve()
+        }
     }
 }
