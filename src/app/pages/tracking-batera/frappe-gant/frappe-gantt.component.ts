aba735653f59ae70f9a4acfee30ff51b3c24f6f0
wida praponco <widapraponco02@gmail.com>
2022-10-23 03:08:45 +0700

    add task interface

--- src/app/pages/tracking-batera/frappe-gant/frappe-gantt.component.ts
+++ src/app/pages/tracking-batera/frappe-gant/frappe-gantt.component.ts
@@ -1,6 +1,21 @@
 import { Component, ViewChild, ElementRef, OnInit, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
 import Gantt from './lib'
 
+export interface Task {
+    id: string;
+    name: string;
+    start: Date;
+    end: Date;
+    price: number;
+    progress_log: ProgressLog[];
+    dependencies?: string;
+}
+
+export interface ProgressLog {
+    progress: string;
+    date: string;
+}
+
 @Component({
     selector: 'f-gantt',
     template: '<svg id="gantt" style="min-height: 100%"></svg>',
@@ -12,60 +27,9 @@ export class FrappeGanttComponent implements OnInit, OnChanges {
 
     @Input() showSCurve: boolean = false
     @Input() viewMode: string = 'Day'
+    @Input() tasks: any[]
 
-    gantt;
-    tasks = [
-        {
-            id: 'Task 1',
-            name: 'Redesign website',
-            start: '2016-1-11',
-            end: '2016-1-21',
-            price: 500,
-            progress_log: [
-                {progress: 10, date: '2016-1-12'},
-                {progress: 50, date: '2016-1-15'},
-                {progress: 100, date: '2016-1-30'},
-            ],
-            // dependencies: 'Task 2, Task 3',
-        },
-        {
-            id: 'Task 2',
-            name: 'Redesign website 2',
-            start: '2016-1-21',
-            end: '2016-1-25',
-            price: 300,
-            progress_log: [
-                {progress: 100, date: '2016-1-24'}
-            ],
-            dependencies: 'Task 1',
-        },
-        {
-            id: 'Task 3',
-            name: 'Redesign website 3',
-            start: '2016-1-25',
-            end: '2016-1-31',
-            price: 200,
-            progress_log: [
-                {progress: 100, date: '2016-1-31'},
-            ],
-            dependencies: 'Task 2',
-        },
-        {
-            id: 'Task 4',
-            name: 'Redesign website',
-            start: '2016-1-11',
-            end: '2016-1-21',
-            price: 100,
-            progress_log: [
-                {progress: 50, date: '2016-1-15'},
-                {progress: 100, date: '2016-1-21'},
-            ],
-            // dependencies: 'Task 2, Task 3',
-        },
-    ].map(t => {
-        return {...t, progress: t.progress_log?.map(p => p.progress).reduce((p,c) => p+c, 0)/(t.progress_log?.length ?? 1) ?? 0}
-    });
-
+    gantt: any;// gantt object
     ngOnInit() {
         this.gantt = new Gantt('#gantt', this.tasks, {});        
     }
