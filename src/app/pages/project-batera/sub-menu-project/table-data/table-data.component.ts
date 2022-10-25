ed4d1609899c2bc3fd0daf8ac8514109c4810aba
Dimasariza <roganda.sihombing11@gmail.com>
2022-10-26 00:34:28 +0700

    update table data project currency

--- src/app/pages/project-batera/sub-menu-project/table-data/table-data.component.ts
+++ src/app/pages/project-batera/sub-menu-project/table-data/table-data.component.ts
@@ -1,22 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FunctionCollection } from '../../../function-collection-batera/function-collection.component';
 
-
-const PROGRESS_DATA = [
-  {name: 'Offhire Days', budget: 14.0067, contract : 0,  actual: ''},
-  {name: 'Owner Exp', budget: 15.9994, contract : 0,  actual: ''},
-  {name: '- Supplies', budget: 18.9984, contract : 0,  actual: ''},
-  {name: '- Services', budget: 20.1797, contract : 0,  actual: ''},
-  {name: '- Class', budget: 20.1797, contract : 0,  actual: ''},
-  {name: '- Other', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Additional Job', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Owner Canceled Job', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Amortization Job', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Depreciation Job', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Yard Cost', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Yard Cancelled Jobs', budget: 20.1797, contract : 0,  actual: ''},
-  {name: 'Total Cost', budget: 20.1797, contract : 0,  actual: ''},
-];
 
 
 @Component({
@@ -25,13 +10,63 @@ const PROGRESS_DATA = [
   styleUrls: [ './table-data.component.scss'
   ]
 })
-export class TableDataComponent implements OnInit {
-
+export class TableDataComponent implements OnChanges, OnInit {
+  @Input() tableData : any
   statusColumn: string[] = ['name', 'Budget', 'Contract', 'Actual'  ];
-  progressData = PROGRESS_DATA
-  constructor() { }
+
+  constructor(public FNCOL : FunctionCollection,
+              public currency : CurrencyPipe
+  ) { }
+  progressData : any 
 
   ngOnInit(): void {
+    this.progressData = 
+    [
+      {name: 'Offhire Days', budget : '', contract : '|', actual : '|'},
+      {name: 'Owner Exp', budget : 0, contract : 0, actual : 0},
+      {name: '- Supplies', budget : 0, contract : 0, actual : 0},
+      {name: '- Services', budget : 0, contract : 0, actual : 0},
+      {name: '- Class', budget : 0, contract : 0, actual : 0},
+      {name: '- Other', budget : 0, contract : 0, actual : 0},
+      {name: 'Additional Job', budget : 0, contract : 0, actual : 0},
+      {name: 'Owner Canceled Job', budget : 0, contract : 0, actual : 0},
+      {name: 'Amortization Job', budget : 0, contract : 0, actual : 0},
+      {name: 'Depreciation Job', budget : 0, contract : 0, actual : 0},
+      {name: 'Yard Cost', budget : 0, contract : 0, actual : 0},
+      {name: 'Yard Cancelled Job', budget : 0, contract : 0, actual : 0},
+      {name: 'Total Cost', budget : 0, contract : 0, actual : 0},
+    ];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const start = 2
+    if(!this.tableData?.work_area) return
+    this.progressData[0].budget = this.tableData?.off_hire_period + ' Days'
+    this.tableData?.work_area.forEach(work => {
+        const {'Price Budget' : budget ,'Price Actual' : actual, 'Price Contract' : contract, category} = work
+        this.progressData[category.id + start].budget += parseInt(budget) 
+        this.progressData[category.id + start].contract += parseInt(contract)
+        this.progressData[category.id + start].actual += parseInt(actual)
+      });
+      
+      for(let i = start ; i <= 5 ; i++) {
+        this.progressData[1].budget += this.progressData[i].budget
+        this.progressData[1].contract += this.progressData[i].contract
+        this.progressData[1].actual += this.progressData[i].actual
+      }
+
+      const usedCurrency = this.FNCOL.convertCurrency(this.tableData.mata_uang)
+      for(let i = start ; i <= 11 ; i++) {
+        this.progressData[12].budget += this.progressData[i].budget
+        this.progressData[12].contract += this.progressData[i].contract
+        this.progressData[12].actual += this.progressData[i].actual
+      }
+      for(let i = 1 ; i <= 12 ; i++) {
+        if(this.progressData[i].budget == 0 || this.progressData[i].contract == 0 || this.progressData[i].actual == 0) continue
+        this.progressData[i].budget = this.currency.transform(this.progressData[i].budget, usedCurrency)
+        this.progressData[i].contract = this.currency.transform(this.progressData[i].contract, usedCurrency)
+        this.progressData[i].actual = this.currency.transform(this.progressData[i].actual, usedCurrency)
+      }
   }
 
 }
